eece3dc9740d92fad27cfc5532470d32caaf764f
rakshit389 <rakshitupadhyay659@gmail.com>
2024-04-23 12:18:16 +0530

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+var items = document.querySelectorAll('.pag-wid-same');
+
+items.forEach(function(item) {
+    var header = item.querySelector('.pag-wid-head');
+    var content = item.querySelector('.pag-wid-dropdown');
+
+    header.addEventListener('click', function() {
+
+        let icon = header.getElementsByTagName('span');
+        icon[0].innerText = icon[0].innerText === 'add' ? icon[0].innerText = 'remove' : icon[0].innerText = 'add' ;
+        content.style.display = content.style.display === 'block' ? content.style.display = 'none' : content.style.display = 'block' ;
+    })
+});
  
